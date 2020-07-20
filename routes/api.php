708df84b97fff46e6d875633dfa5e996c563c603
stevan <stevann.tosic@gmail.com>
2020-07-20 13:37:16 +0200

    add /me endpoint

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function () {
+
+    Route::get('/me', 'UserController@meAction');
 });
 
 Route::post('/register', 'UserRegistrationController@register');
