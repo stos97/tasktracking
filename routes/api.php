06b3a7cd3206803e02eca19f0141cb0d4907d66c
stevan <stevann.tosic@gmail.com>
2020-07-20 12:23:53 +0200

    add login endpoint

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('/register', 'UserRegistrationController@register');
+Route::post('/login', 'LoginController@login');
